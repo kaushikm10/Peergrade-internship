02fe6dbbf02ddfe33de513ea7c9d891de46c3676
mihir <mihir.mehta2@somaiya.edu>
2021-04-09 18:15:25 +0530

    whitenoise

--- peergrade/settings.py
+++ peergrade/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/courses/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = os.path.join(BASE_DIR, STATIC_URL)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
